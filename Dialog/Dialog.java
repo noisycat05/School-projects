8835009fb490dd7fd74c68fc7e1ba733313025e9
Ruben Rodriguez <33643304+noisycat05@users.noreply.github.com>
2018-08-17 14:20:40 -0400

    e

--- Dialog/Dialog.java
+++ Dialog/Dialog.java
@@ -4,7 +4,10 @@ public class Dialog{
 
    public static void main(String[] args) {
       
-      JOptionPane.showMessageDialog(null,"Hello everyone!");
+      JOptionPane.showMessageDialog(null,"Hello human!");
+      String answer = JOptionPane.showInputDialog("How are you?");
+      System.out.println(answer);
+      JOptionPane.showMessageDialog(null,"Thats great! I'm glad your feeling "+answer+" today.");
       
    }
 
