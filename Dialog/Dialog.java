f2305922e5f2f9f548fb6be036d6b6650c2e52a5
Ruben Rodriguez <33643304+noisycat05@users.noreply.github.com>
2018-08-17 13:16:44 -0400

    New lesson

---
+++ Dialog/Dialog.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+public class Dialog{
+
+   public static void main(String[] args) {
+      
+      JOptionPane.showMessageDialog(null,"Hello everyone!");
+      
+   }
+
+}
